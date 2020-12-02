1aac139a014d889da81125a370773c9908f733aa
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-12-01 22:29:31 -0700

    models created

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, 'Please create valid email address']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of FRIENDS on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.slice(0, this.friends.indexOf(0));
+});
+  
+
+// create the USER model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the USER model
+module.exports = User;
